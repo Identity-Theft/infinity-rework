e2e7824703dec5d6b0e93fbdf965b5b0da728090
Identity Theft <liam.fitz1920@gmail.com>
2024-05-13 22:06:58 +1000

    Initial Commit

---
+++ src/main/java/identitytheft/infinityrework/InfinityRework.java
@@ -0,0 +1,16 @@
+package identitytheft.infinityrework;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InfinityRework implements ModInitializer {
+	public static final String MOD_ID = "infinity-rework";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Loading Infinity Rework");
+	}
+}
---
+++ src/main/java/identitytheft/infinityrework/mixin/InfinityEnchantmentMixin.java
@@ -0,0 +1,38 @@
+package identitytheft.infinityrework.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.InfinityEnchantment;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(InfinityEnchantment.class)
+public class InfinityEnchantmentMixin {
+	@Unique
+	public int getMaxLevel() {
+		return 3;
+	}
+
+	@Inject(at = @At("HEAD"), method = "getMinPower", cancellable = true)
+	private void getMinPower(int level, CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(level * 10);
+	}
+
+	@Inject(at = @At("HEAD"), method = "getMaxPower", cancellable = true)
+	private void getMaxPower(int level, CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(level * 20);
+	}
+
+	@Inject(at = @At("HEAD"), method = "canAccept", cancellable = true)
+	private void canAccept(Enchantment other, CallbackInfoReturnable<Boolean> cir) {
+		//noinspection ConstantConditions
+		cir.setReturnValue((Object) this != other);
+	}
+}
