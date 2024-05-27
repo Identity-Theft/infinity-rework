8ad7eeaf3d8f9a1c3f1c70bf2c32d56f61b71147
Identity Theft <liam.fitz1920@gmail.com>
2024-05-27 21:18:31 +1000

    tipped arrows

--- src/main/java/identitytheft/infinityrework/mixin/BowItemMixin.java
+++ src/main/java/identitytheft/infinityrework/mixin/BowItemMixin.java
@@ -3,7 +3,6 @@ package identitytheft.infinityrework.mixin;
 import identitytheft.infinityrework.InfinityRework;
 import identitytheft.infinityrework.InfinityReworkConfig;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
